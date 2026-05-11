ca860999096f739bd0e18c5cc5e8b38a6b89a409
SheikhRaihan1 <shekhrayhan605@gmail.com>
2026-05-11 11:05:21 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+
+echo"Hello World";
+
+print "Hello Print";
+
+print_r("Hello Print r");
+
+// variabble declair
+$firstName= "Sheikh";
+$lastName= "Raihan";
+echo "<br>";
+echo"$firstName $lastName";
+
+echo "<br>";
+// number variable declare
+$num1= 10;
+$num2= 5;
+$sum= $num1 + $num2;
+$sub= $num1 - $num2;
+$mul= $num1 * $num2;
+$div= $num1 / $num2;
+$mod= $num1 % $num2;
+
+echo "Sum is $sum <br>";
+echo "Sub is $sub <br>";
+echo "Multification is $mul <br>";
+echo "Divition is $div <br>";
+echo "Modulas is $mod <br>";
+
+
+?>
+
+<!-- shorthand -->
+ <?= "Hi"?>
